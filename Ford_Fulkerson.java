11e56313e1f43b306243f801a0bd98c50d5ea249
littlesarker <61264159+littlesarker@users.noreply.github.com>
2022-07-09 07:25:52 +0600

    The Ford-Fulkerson algorithm is used to detect maximum flow from start vertex to sink vertex in a given graph.Time Complexity: Time complexity of the above algorithm is O(max_flow * E).
    Time Complexity: Time complexity of the above algorithm is O(max_flow * E).

---
+++ Ford_Fulkerson.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.util.*;
+
+public class Ford_Fulkerson {
+	static final int V = 5;
+
+	boolean bfs(int Graph[][], int s, int t, int p[]) {
+		boolean visited[] = new boolean[V];
+		for (int i = 0; i < V; ++i)
+			visited[i] = false;
+
+		LinkedList<Integer> queue = new LinkedList<Integer>();
+		queue.add(s);
+		visited[s] = true;
+		p[s] = -1;
+
+		while (queue.size() != 0) {
+			int u = queue.poll();
+
+			for (int v = 0; v < V; v++) {
+				if (visited[v] == false && Graph[u][v] > 0) {
+					queue.add(v);
+					p[v] = u;
+					visited[v] = true;
+				}
+			}
+		}
+
+		return (visited[t] == true);
+	}
+
+	public static void main(String[] args) throws java.lang.Exception {
+		// TODO Auto-generated method stub
+
+		int graph[][] = new int[][] { { 0, 8, 0, 0, 3, 0 }, 
+			{ 0, 0, 9, 0, 0, 0 },
+			{ 0, 0, 0, 0, 7, 2 },
+				{ 0, 0, 0, 0, 0, 5 },
+				{ 0, 0, 0, 0, 0, 0 } };
+		Ford_Fulkerson m = new Ford_Fulkerson();
+
+		System.out.println("Max Flow: " + m.fordFulkerson(graph, 0, 4));
+
+	}
+
+	int fordFulkerson(int graph[][], int s, int t) {
+		int u, v;
+		int Graph[][] = new int[V][V];
+
+		for (u = 0; u < V; u++)
+			for (v = 0; v < V; v++)
+				Graph[u][v] = graph[u][v];
+
+		int p[] = new int[V];
+
+		int max_flow = 0;
+
+		while (bfs(Graph, s, t, p)) {
+			int path_flow = Integer.MAX_VALUE;
+			for (v = t; v != s; v = p[v]) {
+				u = p[v];
+				path_flow = Math.min(path_flow, Graph[u][v]);
+			}
+
+			for (v = t; v != s; v = p[v]) {
+				u = p[v];
+				Graph[u][v] -= path_flow;
+				Graph[v][u] += path_flow;
+			}
+
+			max_flow += path_flow;
+		}
+
+		return max_flow;
+	}
+
+}
